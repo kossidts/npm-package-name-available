56d6b9b0a765677934652109e15c7978fb1a7737
Kossi D. T. Saka <kdtsaka@gmail.com>
2022-04-21 23:55:51 +0200

    Improve accuracy

--- index.js
+++ index.js
@@ -3,59 +3,81 @@ const https = require("https");
 const validate_pkg_name = require("validate-npm-package-name");
 
 const npm_package_name_available = names => {
+    // Inforce array
     if (!Array.isArray(names)) {
         names = [names];
     }
 
-    let requests = names.map(name => {
+    // Generate list of names and similar names by removing one punctuation at a time
+    let similar_names = [];
+    for (let name of names) {
+        similar_names.push(name);
+
+        // No need to remove punctuation from the scope if available
+        let unscoped = name.split("/");
+        let prefix = unscoped.length > 1 ? unscoped.shift() + "/" : "";
+        name = unscoped.join("/");
+
+        let name_forward_lookup = name;
+        while (name_forward_lookup.search(/[.\-_]/) != -1) {
+            name_forward_lookup = name_forward_lookup.replace(/[.\-_]/, "");
+            similar_names.push(prefix + name_forward_lookup);
+        }
+
+        let name_backward_lookup = name;
+        while (name_backward_lookup.search(/[.\-_][^.\-_]*$/) != -1) {
+            name_backward_lookup = name_backward_lookup.replace(/^(.*)([.\-_])([^.\-_]*)$/, "$1$3");
+            similar_names.push(prefix + name_backward_lookup);
+        }
+    }
+
+    // Remove duplicates
+    similar_names = new Set(similar_names);
+    similar_names = [...similar_names];
+
+    const pkg_name_available = name => {
+        return new Promise((resolve, reject) => {
+            https
+                .request(`https://www.npmjs.com/package/${name}`, { method: "HEAD" }, res => {
+                    let is_available = false;
+                    if (res.statusCode == 404 || (/^@/.test(name) && res.statusCode == 302)) {
+                        is_available = true;
+                    }
+                    resolve({ [name]: is_available });
+                })
+                .end();
+        });
+    };
+
+    const remove_punctuations = str => (str || "").replace(/[.\-_]/g, "");
+
+    let requests = similar_names.map(name => {
         let valid = validate_pkg_name(name);
         if (!valid.validForNewPackages) {
             return { [name]: false };
         }
 
-        const check_availablitiy = (pkg_name, similar_name) => {
-            if ("undefined" == typeof similar_name) {
-                similar_name = pkg_name;
-            }
-            return new Promise((resolve, reject) => {
-                https
-                    .request(`https://www.npmjs.com/package/${similar_name}`, { method: "HEAD" }, res => {
-                        // console.log("headers:", res.headers);
-                        // console.log("statusCode:", res.statusCode, similar_name, res.headers.location);
-                        let is_available = false;
-                        if (res.statusCode == 404 || (/^@/.test(similar_name) && res.statusCode == 302)) {
-                            is_available = true;
-                        }
-
-                        resolve({ [pkg_name]: is_available });
-                    })
-                    .end();
-            });
-        };
-
-        let no_punctuation_conflict_name = name.replace(/[.\-_]/g, "");
-        if (no_punctuation_conflict_name !== name) {
-            return Promise.all([name, no_punctuation_conflict_name].map(_name => check_availablitiy(name, _name)));
-        }
-
-        return check_availablitiy(name);
+        return pkg_name_available(name);
     });
 
     return Promise.all(requests).then(results => {
-        results = results.map(result => {
-            if (Array.isArray(result)) {
-                let obj = result.shift();
-                let name = Object.keys(obj)[0];
-                // There are max two elements
-                obj[name] = obj[name] && result[0][name];
-                result = obj;
-            }
-            return result;
-        });
-        if (names.length == 1) {
-            results = results[0][names[0]];
+        let end_result = [];
+        for (const name of names) {
+            let is_available = results.reduce((acc, cur) => {
+                let key = Object.keys(cur)[0];
+                if (remove_punctuations(key) == remove_punctuations(name)) {
+                    acc = acc && cur[key];
+                }
+                return acc;
+            }, true);
+            end_result.push({ [name]: is_available });
         }
-        return results;
+
+        if (end_result.length == 1) {
+            return Object.values(end_result[0])[0];
+        }
+
+        return end_result;
     });
 };
 
